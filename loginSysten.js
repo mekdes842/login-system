131b485a33f35a613eaf13468ec108f03bb6d95d
mekdes842 <mekdestesfayey@gmail.com>
2026-02-16 06:07:41 +0300

    Initial commit

---
+++ loginSysten.js
@@ -0,0 +1,8 @@
+var username=prompt("Please enter your username");
+var password=prompt("Please enter your password");
+if(username==="admin" && password==="1234"){
+    console.log("Login Successful")
+}
+else{
+    console.log("Invalid Credentials")
+}
